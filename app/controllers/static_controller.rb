d4522d1883f5d91cb2ea5579129fe0f4db623b54
WinnyDrancourt <winny.morel@orange.fr>
2024-02-12 17:33:14 +0100

    add user show

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,10 +2,16 @@ class StaticController < ApplicationController
   def welcome
     @first_name = params[:first_name]
   end
-  
+
   def team
   end
 
   def contact
+  end
+
+  def users
+    @user = User.find(params[:id])
+    @gossip_count = Gossip.where(user_id: User.find(params[:id])).count
+    @comment_count = Comment.where(user_id: User.find(params[:id])).count
   end
 end
