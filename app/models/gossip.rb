c96898b6f5e1a29eab914c543bc88d2d95345f45
evarellapucky <scherer.alexia@gmail.com>
2024-02-13 14:56:10 +0100

    modified model

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,9 @@
 class Gossip < ApplicationRecord
+  validates :title, 
+    presence: true,
+    length: { minimum: 3, maximum: 14 }
+  validates :content, presence: true
+  
   belongs_to :user
 
   has_many :poly_comments, as: :commentable
