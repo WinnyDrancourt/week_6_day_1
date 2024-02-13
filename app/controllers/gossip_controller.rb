00fc180a6b82515dad51ad218542652571f651d9
lexi <alexiacabanel1@gmail.com>
2024-02-13 15:51:44 +0100

    redirect create

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -10,10 +10,18 @@ class GossipController < ApplicationController
   end
 
   def new
+    @gossip = Gossip.new
   end
 
   def create
-    Gossip.create(user_id: User.all.shuffle.first.id, title: params[:title], content: params[:content])
+    @gossip = Gossip.create(user_id: User.all.shuffle.first.id, title: params[:title], content: params[:content])
+    if @gossip.save
+      puts "potin créé"
+      redirect_to '/'
+    else
+      puts "Error : completez correctement svp"
+      render 'gossip/new'
+    end
   end
 
   def edit
