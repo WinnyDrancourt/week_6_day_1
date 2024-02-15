233a27d43480866ebc5e2e846ed68b69a3aead79
lexi <alexiacabanel1@gmail.com>
2024-02-15 21:26:50 +0100

    style accueil

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -8,7 +8,7 @@ class Gossip < ApplicationRecord
   belongs_to :user
 
   has_many :comment
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   #has_many :poly_comments, as: :commentable
   has_many :join_table_gossip_tags
