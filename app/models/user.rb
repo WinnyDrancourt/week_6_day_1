b3daa15e46fd0a8be76c0c353f11e7f70bf34423
WinnyDrancourt <winny.morel@orange.fr>
2024-02-15 12:34:45 +0100

    fix comment part and add delete comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ class User < ApplicationRecord
   has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
   has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true
+
+  validates :email, confirmation: true
+  validates :email_confirmation, presence: true
+
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  PASSWORD_REQUIREMENTS = /\A
+    (?=.{8,}) # Minimum 8 charactere de longueur
+    (?=.*\d) # Contient au mini 1 chiffre
+    (?=.*[a-z]) # Au mini un lowecase
+    (?=.*[A-Z]) # Au mini un uppercase
+    (?=.[[:^alnum:]]) # Au mini 1 symbole
+    /x
+  validates :password, format: PASSWORD_REQUIREMENTS
+
 end
